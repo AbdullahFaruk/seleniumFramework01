79cb1ca8df9a730ff4fae125526ec0d3ae056915
aalfaruk <abdullah.faruk85@gmail.com>
2016-09-24 14:08:52 -0400

    adding the Log4J

---
+++ src/main/java/com/paxotech/LinksOnHomePage.java
@@ -0,0 +1,14 @@
+package com.paxotech;
+
+
+
+/**
+ * Created by shawon on 9/9/16.
+ */
+public enum LinksOnHomePage {
+
+    HOMEPAGE ();
+
+
+
+}
---
+++ src/main/java/com/paxovision/conceptual/ArrayBounds.java
@@ -0,0 +1,21 @@
+package com.paxovision.conceptual;
+
+
+
+/**
+ * Created by shawon on 9/23/16.
+ */
+public class ArrayBounds {
+
+
+    public static void main (String [] args){
+
+        //create Array and declare an array
+
+
+
+
+    }
+
+
+}
